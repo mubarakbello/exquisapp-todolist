d161425d839d5820c0542f4f4fc528b494ab8c6e
Mubarak Bello <bellomuba.rak0@gmail.com>
2020-01-21 17:35:18 +0100

    Added other route controllers

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -62,7 +62,7 @@ module.exports = {
       if (!data.length) {
         return res.json({
           error: true,
-          message: 'listId passed non-existent'
+          message: 'TodoList with listId passed non-existent'
         });
       }
 
@@ -80,6 +80,85 @@ module.exports = {
           message: `New todo list item with Id ${data._id} added`,
           data: data
         });
+      });
+    });
+  },
+
+  updateTodoItem: (req, res) => {
+    const itemId = req.params.item_id;
+    const { title } = req.body;
+
+    if (!itemId || !title) {
+      return res.json({
+        error: true,
+        message: 'Both itemId and todoItem title to update must be passed'
+      });
+    }
+
+    TodoItems.findOneAndUpdate({ itemId }, { title }, { new: true }, (err, doc) => {
+      if (err) {
+        return res.json({
+          error: true,
+          message: err.message
+        });
+      }
+
+      res.json({
+        error: false,
+        message: `Todo list item with Id ${data._id} updated`,
+        data: data
+      });
+    })
+  },
+
+  deleteTodoListById: (req, res) => {
+    const listId = req.params.list_id;
+
+    if (!listId) {
+      return res.json({
+        error: true,
+        message: 'listId of Todo List to delete must be passed'
+      });
+    }
+
+    TodoLists.findByIdAndDelete(listId, (err, data) => {
+      if (!data) {
+        return res.json({
+          error: true,
+          message: 'TodoList with listId passed non-existent'
+        });
+      }
+
+      res.json({
+        error: false,
+        message: `TodoList with Id ${data._id} deleted`,
+        data: data
+      });
+    })
+  },
+
+  deleteTodoItemById: (req, res) => {
+    const itemId = req.params.item_id;
+
+    if (!itemId) {
+      return res.json({
+        error: true,
+        message: 'itemId of Todo List item to delete must be passed'
+      });
+    }
+
+    TodoItems.findByIdAndDelete(itemId, (err, data) => {
+      if (!data) {
+        return res.json({
+          error: true,
+          message: 'TodoItem with itemId passed non-existent'
+        });
+      }
+
+      res.json({
+        error: false,
+        message: `TodoItem with Id ${data._id} deleted`,
+        data: data
       });
     })
   }
