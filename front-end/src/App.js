d913e2e969d50e21b387e835b09808374b4d4c88
Mubarak Bello <bellomuba.rak0@gmail.com>
2020-01-21 18:19:59 +0100

    Temp commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Container, Row, Col, Navbar, Card} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <header>TodoApp</header>
+      <Navbar sticky="top" bg="primary" variant="dark">
+        <Navbar.Brand href="#home">TodoApp</Navbar.Brand>
+      </Navbar>
+      <Container style={{marginTop: "40px"}}>
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <Card body>This is some text within a card body.</Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
